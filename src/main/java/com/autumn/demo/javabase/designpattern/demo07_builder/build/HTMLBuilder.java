2654a759f0fd7a168896344a54d2aa29447b33b1
1455653761 <1455653761@qq.com>
2018-11-08 16:27:03 +0800

    20181108 设计模式-Builder模式

---
+++ src/main/java/com/autumn/demo/javabase/designpattern/demo07_builder/build/HTMLBuilder.java
@@ -0,0 +1,51 @@
+package com.autumn.demo.javabase.designpattern.demo07_builder.build;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Stream;
+
+/**
+ * @author xql132@zcsmart.com
+ * @date 2018/11/8 15:29
+ * @description 使用HTML编写文档
+ */
+public class HTMLBuilder extends Builder {
+    private String filename; // 文件名
+    private PrintWriter writer; // 编写文件
+
+    @Override
+    public void makeTitle(String title) {
+        filename = title +".html";
+        try {
+            writer = new PrintWriter(new FileWriter(filename)); // 生产PringtWriter
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writer.println("<html><head><title>" + title + "</title></head><body>");
+        writer.println("<h1>" + title + "</h1>");
+    }
+
+    @Override
+    public void makeString(String str) {
+        writer.println("<p>" + str + "</p>");
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+        writer.println("<ul>");
+        Stream<String> stream = Stream.of(items);
+        stream.forEach(a -> {writer.println("<li>"+a+"</li>");});
+        writer.println("</ul>");
+    }
+
+    @Override
+    public void close() {
+        writer.print("</body></html>");
+        writer.close();
+    }
+
+    public String getResult() {
+        return filename;  // 返回文件名
+    }
+}
---
+++ src/main/java/com/autumn/demo/javabase/designpattern/readme.txt
@@ -0,0 +1,11 @@
+designpattern包下放的是23种设计模式的例子
+
+demo07_builder Builder模式-组装复杂的实例
+Builder(建造者)
+Builder角色负责定义用于生成实例的接口(API), Builder角色中准备了用于生成实例的方法.
+ConcreteBuilder(具体的建造者)
+ConcreteBuilder角色是负责实现Builder角色的接口的类, 这里定义了在生成实例时实际被调用的方法,
+此外,在ConcreteBuilder角色中还定义了最终生成结果的方法
+Director(监工)
+Director角色负责使用Builder角色的接口来生成实例, 不依赖concreteBuilder角色.为了确保不论ConcreteBuilder
+角色是如何被定义的, Director角色都能正常工作, 它只调用在Builder角色中被定义的方法.
