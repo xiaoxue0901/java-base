1a267e2e2b12595874c2dd046469e7d77756fb16
薛秋兰 <xql132@zcsmart.com>
2019-05-10 20:28:13 +0800

    20190430 设计模式-Iterator

---
+++ src/main/java/com/autumn/demo/javabase/designpattern/demo02_adapter/Adapter/Sample1/PrintBanner.java
@@ -0,0 +1,23 @@
+package com.autumn.demo.javabase.designpattern.demo02_adapter.Adapter.Sample1;
+
+/**
+ * 1. 继承Banner
+ * 2. 实现Print
+ */
+public class PrintBanner extends Banner implements Print {
+
+    // 构造函数:
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    // 实现Print接口方法
+    public void printWeak() {
+        showWithParen();
+    }
+
+    // 实现Print接口方法
+    public void printStrong() {
+        showWithAster();
+    }
+}
