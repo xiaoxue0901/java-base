64e021ef2c567d6f59843381d02384a744b1b82e
薛秋兰 <xql132@zcsmart.com>
2019-12-19 20:57:56 +0800

    20191016 二分查找

---
+++ src/main/java/com/autumn/demo/javabase/base/chapter3/C8ControlProcess.java
@@ -0,0 +1,60 @@
+package com.autumn.demo.javabase.base.chapter3;
+
+/**
+ * @author xql132@zcsmart.com
+ * @date 2019/10/16 20:23
+ * @description 控制流程
+ */
+public class C8ControlProcess {
+
+
+}
+
+class BreakProcess {
+
+    public static void breakProcess_break() {
+        // 语句 break
+        int i = 0;
+        while (true) {
+            i = +1;
+            if (i == 14) {
+                break;
+            }
+        }
+
+        // 带标签的break语句
+        a:
+        for (; ; ) {
+            ++i;
+            if (i > 20) {
+                while (true) {
+                    if (i == 32) {
+                        break a;
+                    }
+                }
+            }
+        }
+
+        // 语句continue:
+        int j = 21;
+        while (true) {
+            --j;
+            if (j % 2 == 0) {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        // 带标签语句continue:
+        b:
+        for (;;) {
+            while (true) {
+                --j;
+                if (j % 2 == 1) {
+                    continue b;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/autumn/demo/javabase/reflect/BodyAnnonation.java
@@ -0,0 +1,27 @@
+package com.autumn.demo.javabase.reflect;
+
+import java.lang.annotation.*;
+
+/**
+ * @author xql132@zcsmart.com
+ * @date 2019/11/27 17:10
+ * @description
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BodyAnnonation {
+
+    /**
+     * 长度: 获取属性长度
+     * @return
+     */
+    int len();
+
+    /**
+     * 编码: 获取属性编码格式
+     * @return
+     */
+    String code() default "HEX";
+
+}
