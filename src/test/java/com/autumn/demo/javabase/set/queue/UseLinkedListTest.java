f068fbe10d8786be41c0b01b1f4d0564d3f5edfc
xql132 <xql132@zcsmart.com>
2021-01-04 18:31:39 +0800

    20200104 集合

---
+++ src/test/java/com/autumn/demo/javabase/set/queue/UseLinkedListTest.java
@@ -0,0 +1,24 @@
+package com.autumn.demo.javabase.set.queue;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author xql132@zcsmart.com
+ * @date 2021/1/4
+ * @time 14:49
+ * @description
+ */
+public class UseLinkedListTest {
+
+    @Test
+    public void useIterator() {
+        UseLinkedList.useIterator();
+    }
+
+    @Test
+    public void useListIterator() {
+        UseLinkedList.useListIterator();
+    }
+}
